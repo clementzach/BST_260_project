4bbb9755f232bab20ed74a1c510e84fde644f6ad
Zachary Clement <clementzach@icloud.com>
2021-10-29 15:58:35 -0400

    Added .gitignore, web scraped injury data

--- webscraping.R
+++ webscraping.R
@@ -2,6 +2,7 @@ library(rvest)
 library(stringr)
 library(dplyr)
 
+library(lubridate)
 
 base_url <- 'https://www.lineups.com/nfl/rosters'
 
@@ -53,6 +54,81 @@ height_to_inches <- function(str){
   
 }
 
-players_df <- players_df %>% mutate(height_inches = height_to_inches(Height), ranking_numeric = str_extract(Ranking, pattern = "[:digit:]+") %>% as.numeric())
+players_df <- players_df %>% 
+  mutate(height_inches = height_to_inches(Height), 
+         ranking_numeric = str_extract(Ranking, pattern = "[:digit:]+") %>% as.numeric(),
+         Birthday_string = Birthday,
+         Birthday = mdy(Birthday))
+
+
+is_even <- str_split(players_df$Name, pattern = ' ') %>% sapply(FUN = length) 
+max(is_even)
+min(is_even)
+sum(is_even == 5) #none are odd, good.
+
+full_name <- function(in_string){
+  words_vec <- str_split(in_string, pattern = ' ')[[1]]
+  name <- paste(words_vec[1:(length(words_vec)/2)], collapse = ' ')
+  return(name)
+}
+
+short_name <- function(in_string){
+  words_vec <- str_split(in_string, pattern = ' ')[[1]]
+  name <- paste(words_vec[(length(words_vec)/2 +1):length(words_vec)], collapse = ' ')
+  return(name)
+}
+  
+
+players_df <- players_df %>% mutate(Full_Name = sapply(Name, FUN = full_name),
+                                    Short_Name = sapply(Name, FUN = short_name))
+
+
 
 write.csv(players_df, file = "nfl_roster.csv")
+
+
+
+
+
+
+injuries_url <- "https://www.cbssports.com/nfl/injuries/daily/"
+
+website <- read_html(injuries_url) 
+
+tables <- website %>% html_elements('.TableBase') %>% html_table()
+
+names <- website %>% html_elements('.TableBase-title')  %>% html_text() %>% str_trim()
+
+injuries_df <- data.frame(Team = c(), Player = c(), Position = c(), Injury = c(), "Injury Status" = c(), Date = c())
+
+for (i in 1:length(names)){
+  current_df <- tables[[i]] %>% mutate(Date = names[i])
+  injuries_df <- rbind(injuries_df, current_df)
+  
+}
+
+short_name <- function(input_string){
+  v <- str_split(input_string, '\n')
+  name <- v[[1]][1] %>% str_trim()
+  return(name)
+}
+
+
+full_name <- function(input_string){
+  v <- str_split(input_string, '\n')
+  name <- v[[1]][length(v[[1]])] %>% str_trim()
+  return(name)
+}
+
+injuries_df <- injuries_df %>% mutate(Date = mdy(Date),
+                                      Short_Name = sapply(Player, short_name),
+                                      Full_Name = sapply(Player, full_name)
+                                      )
+
+
+
+write.csv(injuries_df, file = "injuries.csv")
+
+
+
+

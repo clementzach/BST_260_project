06453dc22e711b01b269a1ce9b52e84947963245
willow-duffell <wduffell@hsph.harvard.edu>
2021-11-30 09:45:13 -0500

    Created Shiny app

--- EDA/Q2/app.R
+++ EDA/Q2/app.R
@@ -12,22 +12,24 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+    theme = shinythemes::shinytheme("darkly"),
+    
     # Application title
     titlePanel("Injury Counts in the NFL"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("age",
+                        "Age:",
+                        min = 21,
+                        max = 75,
+                        value = 37)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("agehist")
         )
     )
 )
@@ -35,15 +37,28 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$agehist <- renderPlot({
+        injury_age %>% 
+            group_by(bodypart) %>%
+            filter(age == input$age) %>%
+            summarise(counts = sum(counts)) %>%
+            ggplot(aes(x = reorder(bodypart, - counts), y = counts)) +
+            geom_col() +
+            theme(
+                axis.title.x = element_text(size = 15),
+                axis.title.y = element_text(size = 15),
+                title = element_text(size = 22),
+                plot.title = element_text(hjust = 0.5)
+            ) +
+            xlab("Injuries") +
+            ylab("Count") +
+            ggtitle("Distriubtion of Injuries for NFL players")
     })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+#Still need to fix axis & title label to include input$age
+#Change theme

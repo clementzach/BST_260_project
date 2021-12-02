33bdf4909d46a9ef36c068644908192d820bd8c6
willow-duffell <wduffell@hsph.harvard.edu>
2021-12-02 13:01:02 -0500

    Fixed Shiny app!

--- EDA/Q2/app.R
+++ EDA/Q2/app.R
@@ -23,23 +23,77 @@ ui <- fluidPage(
     
     # Application title
     titlePanel("Injury Counts in the NFL"),
+    
+    br(), # add some space
 
-    # Sidebar 
-    sidebarLayout(
+    
+    p("This Shiny app uses a dataset related 
+        to NFL injuries to examine the distribution 
+        of different injury types among NFL football players."),
+    
+    br(), 
+    
+    # 1st row
+    fluidRow(
         
-        #Widget for selection
-        sidebarPanel(
-            #Radio buttons that allow the user to select one
-            radioButtons("position", label = "Select Category of Players:",
-                         choices = c("All Players", "Offense", "Defense"))
-        ),
+        #Sidebar
+        sidebarLayout(
+            
+            #Widget for selection
+            sidebarPanel(
+                
+                # Explanatory text
+                column(6, p("Let's look at the distribution based on broad position")
+                ),
+                
+                #Radio buttons that allow the user to select one
+                column(6, radioButtons("position", label = "Select Category of Players:",
+                             choices = c("All Players", "Offense", "Defense"))
+                )
+            ),
+            
+            mainPanel(
+                # Plot
+                column(12, plotOutput("poshist")
+                )
+            )
+        )
+    ),
+    
+    br(),
 
-        # Main panel
-        mainPanel(
-           plotOutput("poshist")
+    # 2nd row
+    fluidRow(
+        
+        #Sidebar
+        sidebarLayout(
+            
+            #Widget for selection
+            sidebarPanel(
+                
+                # Explanatory text
+                column(6, p("Now let's look just at the specific Offensive Positions")
+                ),
+                
+                
+                # Drop down menu for only offensive positions
+                column(6, selectInput(inputId = "off", label = "Choose an Offensive Position",
+                            choices = c(Lineman = "OL", QuarterBack = "QB", RunningBack = "RB", TightEnd = "TE", 
+                                        WideReciever = "WR", Kicker = "K", Punter = "P"))
+                )
+                
+            ),
+            
+            mainPanel(
+                # Plot
+                column(12, plotOutput("offhist") 
+                )
+            )
         )
     )
 )
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -51,6 +105,7 @@ server <- function(input, output) {
                 summarise(counts = sum(counts)) %>%
                 ggplot(aes(x = reorder(bodypart, - counts), y = counts)) +
                 geom_col() +
+                scale_y_continuous(limits = c(0, 10000)) +
                 theme(
                     axis.title.x = element_text(size = 15),
                     axis.title.y = element_text(size = 15),
@@ -61,12 +116,13 @@ server <- function(input, output) {
                 ylab("Count") +
                 ggtitle("Distriubtion of Injuries for NFL players")
             
-        } else if(input$position == "Offense" | "Defense") {
-            str_to_lower(input$position) %>%
+        } else if(input$position == "Offense" | input$position == "Defense") {
+            eval(parse(text = str_to_lower(input$position))) %>%
                 group_by(bodypart) %>%
                 summarise(counts = sum(counts)) %>%
                 ggplot(aes(x = reorder(bodypart, - counts), y = counts)) +
                 geom_col() +
+                scale_y_continuous(limits = c(0, 10000)) +
                 theme(
                     axis.title.x = element_text(size = 15),
                     axis.title.y = element_text(size = 15),
@@ -79,6 +135,32 @@ server <- function(input, output) {
             
         }
     })
+    
+    
+    output$offhist <- renderPlot({
+        
+        offense %>% 
+            filter(position_id == input$off) %>%
+            group_by(bodypart) %>%
+            summarise(counts = sum(counts)) %>%
+            ggplot(aes(x = reorder(bodypart, -counts), y = counts)) +
+            geom_col() +
+            scale_y_continuous(limits = c(0, 1500)) +
+            theme(
+                axis.title.x = element_text(size = 15),
+                axis.title.y = element_text(size = 15),
+                title = element_text(size = 22),
+                plot.title = element_text(hjust = 0.5)
+            ) +
+            xlab("Body Part") +
+            ylab("Count") +
+            ggtitle("Distriubtion of Offensive Injuries")
+        
+        
+    })
+    
+    
+    
 }
 
 # Run the application 
